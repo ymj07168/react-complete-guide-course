ce839b25261703d680c7cf6a6b952f1e25c2c022
이효원 <ymj07168@gmail.com>
2024-01-13 21:59:29 +0900

    feat: add loading state

--- summary-project/src/components/PostsList.jsx
+++ summary-project/src/components/PostsList.jsx
@@ -7,12 +7,15 @@ import { useEffect } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -37,18 +40,20 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 ? (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post, index) => (
             <Post key={index} author={post.author} body={post.body} />
           ))}
         </ul>
-      ) : (
+      )}
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       )}
+      {isFetching && <p>Loading posts...</p>}
     </>
   );
 }
